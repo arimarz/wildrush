db56e6c80988ab64dd46d9ac20f3197f61954c86
Ari <aritheriver@gmail.com>
2024-05-17 11:40:49 -0700

    fixing some errors

--- providers/urls.py
+++ providers/urls.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 from .views import (
-    ProvidersListView, ProvidersDetailView,
-    ProvidersCreateView, ProvidersUpdateView, ProvidersDeleteView
+    ProviderListView, ProviderDetailView,
+    ProviderCreateView, ProviderUpdateView, ProviderDeleteView
 )
 
 app_name = 'providers'
 
 urlpatterns = [
-    path('', ProvidersListView.as_view(), name='provider_list'),
-    path('<int:pk>/', ProvidersDetailView.as_view(), name='provider_detail'),
-    path('create/', ProvidersCreateView.as_view(), name='provider_create'),
-    path('<int:pk>/update/', ProvidersUpdateView.as_view(), name='provider_update'),
-    path('<int:pk>/delete/', ProvidersDeleteView.as_view(), name='provider_delete'),
+    path('', ProviderListView.as_view(), name='provider_list'),
+    path('<int:pk>/', ProviderDetailView.as_view(), name='provider_detail'),
+    path('create/', ProviderCreateView.as_view(), name='provider_create'),
+    path('<int:pk>/update/', ProviderUpdateView.as_view(), name='provider_update'),
+    path('<int:pk>/delete/', ProviderDeleteView.as_view(), name='provider_delete'),
 ]
